d695eaed53c419c33b2c4b09a1a15653b8b03a72
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-08 13:24:16 +0200

    ajout des tags sur la radio

--- src/components/RadioList/Radio.js
+++ src/components/RadioList/Radio.js
@@ -1,10 +1,19 @@
 const Radio = ({handleSound, radio}) => {
-  const {name, url, favicon} = radio;
+  const {name, url, favicon,tags} = radio;
 
   return(
     <div className="radiosList__radio" onClick={() => handleSound(url, name)}>
       { favicon && <img className="radiosList__radio__favicon" src={favicon} /> }
-      <div className="radiosList__radio__name">{name}</div>
+      <div className="radiosList__radio__content">
+        <div className="radiosList__radio__content__name">{name}</div>
+        <div className="radiosList__radio__content__tags">
+          {
+            tags.split(',').map((tag) => (
+              <div className="radiosList__radio__content__tags__tag">{tag}</div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 };
