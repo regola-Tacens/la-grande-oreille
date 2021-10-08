f26322526c2ad7743807905a7961ee844099daae
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-08 17:08:31 +0200

    ajout logo ear et subtitle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
 import './header.scss';
 import { useSelector } from 'react-redux';
+import ear from './ear.svg';
 
 const Header = () => {
   const isLoading = useSelector((state) => state.isLoading);
   return(
     <header className="header">
+      <img className="header__image" src={ear} />
+      <div className="header__image__circle"></div>
       <h1 className="header__title">La Grande Oreille</h1>
+      <p className="header__subtitle">-- plus de 28000 radios stream !! --</p>
       {isLoading && <div className='loader'></div>}
     </header>
   );
